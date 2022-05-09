9c9710be3f41e83efef6f7f6579c718d1461fb27
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-05-09 11:31:19 +0530

    ci: :rocket: lol : main

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.port || 3000, function (req, res) {
+app.listen(process.env.PORT || 3000, function (req, res) {
   console.log("Server listening on http://localhost:3000");
 });
